3f4cba33a239dc4ac42ba2f47a3c3559a705f6ac
Erkinov19 <mukhammad.erkinov@bk.ru>
2022-07-27 17:49:11 +0500

    Initial commit

---
+++ Websocket/src/main/java/co/uk/codeyog/Websocket/WebsocketApplication.java
@@ -0,0 +1,13 @@
+package co.uk.codeyog.Websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketApplication.class, args);
+	}
+
+}
---
+++ Websocket/src/main/java/model/ChatMessage.java
@@ -0,0 +1,20 @@
+package model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class ChatMessage {
+
+    private MessageType type;
+
+    private String content;
+
+    private String sender;
+
+    private String time;
+
+}
